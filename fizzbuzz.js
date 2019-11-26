0c6540f135d496f00560c21c67ccec1b4bec7e4c
Sandra Galilejeva <sandra.galilejeva@gmail.com>
2019-11-26 20:38:42 +0200

    added  new div and p

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,11 +1,23 @@
 console.log("Running my FIZZ BUZZ!");
 
-const app = document.querySelector("#app'); 
+function main() {
+    console.log("Running Main");
+    const app = document.querySelector('#app');
 
-// add a new div
-const newDiv = document.createElement('div');
-newDiv.innerText = "I am new Div";
-app.appendChild(newDiv);
+    //add a new div inside app element
+    const newDiv = document.createElement('div');
+    newDiv.innerText = "I am a new div";
+    app.appendChild(newDiv);
+
+    app.appendChild(document.createElement('p'));
+
+    const mypar = app.querySelector('p');
+    mypar.innerText = "Lorem Ipsum";
+    // mypar.style.backgroundColor = "red";
+    mypar.classList.add("warning-text");
+    mypar.classList.add("bold-text");
+    mypar.id = "hopefully-unique";
+}
 
 
 
