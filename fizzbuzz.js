9600554bd34c955df0fca284365e40290b69c360
Sandra Galilejeva <sandra.galilejeva@gmail.com>
2019-11-26 20:22:43 +0200

    added div and query Selectors

---
+++ fizzbuzz.js
@@ -0,0 +1,13 @@
+console.log("Running my FIZZ BUZZ!");
+
+const app = document.querySelector("#app'); 
+
+// add a new div
+const newDiv = document.createElement('div');
+newDiv.innerText = "I am new Div";
+app.appendChild(newDiv);
+
+
+
+
+
