07eea0548640750fc9c39c3795e7fbca87f531ed
Sandra Galilejeva <sandra.galilejeva@gmail.com>
2019-11-26 21:08:10 +0200

    added classList and Inner text to mypar

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,23 +1,22 @@
 console.log("Running my FIZZ BUZZ!");
 
-function main() {
-    console.log("Running Main");
-    const app = document.querySelector('#app');
+const app = document.querySelector('#app');
 
-    //add a new div inside app element
-    const newDiv = document.createElement('div');
-    newDiv.innerText = "I am a new div";
-    app.appendChild(newDiv);
+const newDiv = document.createElement('div');
+newDiv.innerText = "I am a new Div";
+app.appendChild(newDiv);
 
-    app.appendChild(document.createElement('p'));
+app.appendChild(document.createElement('p'));
 
-    const mypar = app.querySelector('p');
-    mypar.innerText = "Lorem Ipsum";
-    // mypar.style.backgroundColor = "red";
-    mypar.classList.add("warning-text");
-    mypar.classList.add("bold-text");
-    mypar.id = "hopefully-unique";
-}
+const mypar = app.querySelector('p');
+mypar.innerText = "Lorem Ipsem";
+
+// mypar.style.backgroundColor = "red";
+mypar.classList.add("warning-text");
+
+mypar.id = "hopefully-unique";
+
+
 
 
 
