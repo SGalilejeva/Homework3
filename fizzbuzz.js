e4e8aeb1c8cc8f9d2908fc9dafecc1f95eb45417
Sandra Galilejeva <sandra.galilejeva@gmail.com>
2019-11-27 18:01:16 +0200

    added if else loop

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -29,6 +29,10 @@ addElement(app, "mynewelement2", "p", "Lorems");
 
 for (let i = 1; i < 101; i++) {
     addElement(app, "myid" + i, "div", "MyText" + i);
+    if (i % 15 === 0) console.log("Fizz Buzz");
+    else if (i % 3 === 0) console.log("Fizz");
+    else if (i % 5 === 0) console.log("Buzz");
+    else console.log(i);
 }
 
 
